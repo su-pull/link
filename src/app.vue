c667e7fb3475756ae178cd85cac3a4c240e9a2d1
sou <ocelot2828@gmail.com>
2023-05-29 15:56:31 +0900

    feat(app): add pageTransition style

--- src/app.vue
+++ src/app.vue
@@ -3,3 +3,15 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<style>
+  .page-enter-active,
+  .page-leave-active {
+    transition: all 0.4s;
+  }
+  .page-enter-from,
+  .page-leave-to {
+    filter: blur(1rem);
+    opacity: 0;
+  }
+</style>
