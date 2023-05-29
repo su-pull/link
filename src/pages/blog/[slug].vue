475b55dee07c167bea72fe78799eaf0d1de2d9ef
sou <ocelot2828@gmail.com>
2023-05-29 22:39:11 +0900

    feat(blog): add articles & detail

---
+++ src/pages/blog/[slug].vue
@@ -0,0 +1,7 @@
+<template>
+  <ContentDoc v-slot="{ doc }">
+    <time>{{ doc.date }}</time>
+    <h1>{{ doc.title }}</h1>
+    <ContentRenderer :key="doc.date" :value="doc" />
+  </ContentDoc>
+</template>
---
+++ src/pages/blog/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <ContentList v-slot="{ list }">
+    <article v-for="page in list" :key="page._path">
+      <nuxt-link :to="page._path">
+        <h2>{{ page.title }}</h2>
+      </nuxt-link>
+    </article>
+  </ContentList>
+</template>
