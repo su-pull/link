3534c95b7b317b8aac0eb705f004b53e40bb1161
sou <ocelot2828@gmail.com>
2023-05-29 16:11:50 +0900

    refactor(Container): change name to Animate

---
+++ src/components/Container.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class="container"><slot /></main>
+</template>
+
+<style scoped lang="scss">
+  .container {
+    padding: 120px 0;
+    text-align: left;
+  }
+  ul {
+    margin: 0 16px;
+  }
+
+  li {
+    margin: 8px 14px;
+    word-break: keep-all;
+    list-style-type: disc;
+  }
+  @media screen and (max-width: 739.98px) {
+    .container {
+      padding: 120px 16px;
+    }
+  }
+</style>
