262bfc9b65d6832a2307f8392d768c591c2f6155
S.Watanabe <ocelot2828@gmail.com>
2023-06-28 20:13:35 +0900

    fix: fix

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,7 +5,6 @@
       <Meta name="description" content="My app top page" />
     </Head>
     <h1>Top Page</h1>
-
     <p>
       my personal site.<br />It's embarrassing so please don't look too much.
     </p>
